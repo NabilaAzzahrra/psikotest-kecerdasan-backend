e2bef0c31959ad4b6b1f1c65dcdd02a352d6c694
NabilaAzzahrra <zahrrranabill11@gmail.com>
2024-05-02 19:32:14 +0700

    bismillah

--- seeders/20240502090857-questions.js
+++ seeders/20240502090857-questions.js
@@ -1,7 +1,7 @@
 'use strict';
 
 /** @type {import('sequelize-cli')Migration} */
-moduleexports = {
+module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Questions', [
       [
