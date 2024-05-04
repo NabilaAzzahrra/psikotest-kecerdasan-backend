0230c7fb204375964515f1d69832774bb4e9f183
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-05-04 10:34:46 +0700

    view

--- routes/hasils.js
+++ routes/hasils.js
@@ -10,32 +10,7 @@ router.get('/', async (req, res) => {
                 exclude: "id",
             }
         });
-        const workbook = new ExcelJS.Workbook();
-        const sheet = workbook.addWorksheet('Hasil Kecerdasan');
-
-        // Add some dummy data to the sheet
-        sheet.addRow(['No.','Sekolah', 'Kelas', 'Nama Lengkap', 'No. Telpon', 'Kecerdasan']);
-
-        results.forEach((result, index) => {
-            sheet.addRow([
-                index + 1,
-                `${result.school}`,
-                `${result.classes}`,
-                `${result.name_user}`,
-                `${result.phone}`,
-                result.jenis_kecerdasan
-            ]);
-        });
-
-        // Set content type and disposition
-        res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-        res.setHeader('Content-Disposition', 'attachment; filename="example.xlsx"');
-
-        // Save workbook to a buffer
-        const buffer = await workbook.xlsx.writeBuffer();
-
-        // Send the buffer as response
-        res.send(buffer);
+        return res.json(results);
     } catch (error) {
         console.log(error);
         res.status(500).send('Internal Server Error');
@@ -51,8 +26,7 @@ router.get('/:idUser', async (req, res) => {
             },
             where: {
                 id_user: req.params.idUser,
-            },
-            order: [['total', 'DESC']]
+            }
         });
         return res.json(hasils);
     } catch (error) {
