817df0acfca4286a73d5525c2c61db82fd17520a
NabilaAzzahrra <zahrrranabill11@gmail.com>
2024-07-18 11:39:53 +0700

    upgrade 1

--- routes/hasils.js
+++ routes/hasils.js
@@ -25,47 +25,24 @@ router.get('/download', async (req, res) => {
                 exclude: "id",
             }
         });
-
+        
         const workbook = new ExcelJS.Workbook();
         const sheet = workbook.addWorksheet('Hasil Kecerdasan');
-
-        // sheet.addRow(['No.', 'Sekolah', 'Kelas', 'Nama Lengkap', 'No. Telpon', 'Kecerdasan 1', 'Kecerdasan 2', 'Jurusan']);
+        
         sheet.addRow(['No.', 'Sekolah', 'Kelas', 'Nama Lengkap', 'No. Telpon', 'Kecerdasan']);
-
+        
         const groupedResults = [];
-
+        
         results.forEach((result) => {
             const hasil = groupedResults.find((group) => group.id_user == result.id_user);
             if (hasil) {
                 hasil.agilities.push({
                     jenis_kecerdasan: result.jenis_kecerdasan,
-                    jurusan: result.jurusan
                 });
-
-                const jurusanOne = hasil.agilities[0].jurusan.split(',');
-                const jurusanTwo = hasil.agilities[1].jurusan.split(',');
-
-                let jurusan = null;
-                
-                if (jurusanOne.length == 1 || jurusanTwo.length == 1) {
-                    if (jurusanOne.length == 1) {
-                        jurusan = jurusanOne[0];
-                    }
-                    if (jurusanTwo.length == 1) {
-                        jurusan = jurusanTwo[0];
-                    }
-                } else {
-                    let result = [];
-                    for (const jurusan of jurusanOne) {
-                        if (jurusanTwo.includes(jurusan)) {
-                            result.push(jurusan);
-                        }
-                    }
-                    jurusan = result[0];
-                }
-
-                hasil.recommendation = jurusan;
-
+        
+                // Assuming you want to keep only the first intelligence type
+                hasil.agilities = [hasil.agilities[0]];
+        
             } else {
                 groupedResults.push({
                     id_user: result.id_user,
@@ -75,13 +52,11 @@ router.get('/download', async (req, res) => {
                     classes: result.classes,
                     agilities: [{
                         jenis_kecerdasan: result.jenis_kecerdasan,
-                        // jurusan: result.jurusan
                     }],
-                    recommendation: 'Tidak diketahui'
                 });
             }
         });
-
+        
         groupedResults.forEach((groupedResult, index) => {
             sheet.addRow([
                 index + 1,
@@ -90,16 +65,15 @@ router.get('/download', async (req, res) => {
                 `${groupedResult.name}`,
                 `${groupedResult.phone}`,
                 `${groupedResult.agilities[0].jenis_kecerdasan}`,
-                // `${groupedResult.agilities[1].jenis_kecerdasan}`,
-                `${groupedResult.recommendation}`,
             ]);
         });
-
+        
         res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         res.setHeader('Content-Disposition', 'attachment; filename="example.xlsx"');
-
+        
         const buffer = await workbook.xlsx.writeBuffer();
         res.send(buffer);
+        
     } catch (error) {
         console.log(error);
         res.status(500).send('Internal Server Error');
